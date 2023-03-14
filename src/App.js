5458c8dd6c95c995d715dff406e0122de304b830
Ilmaooo <ilmaogresevic1@gmail.com>
2023-03-14 22:12:46 +0100

    pages and routes created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CoursePage from "./pages/CoursePage";
+import ForgotPassword from "./pages/ForgotPassword";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+
 function App() {
   return (
     <>
-      <h1>hello world</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/log-in" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/course-page" element={<CoursePage />} />
+        </Routes>
+      </Router>
     </>
   );
 }
