cae8ac8fd857e87787ff21712c181d7aae1fb98f
Ilmaooo <ilmaogresevic1@gmail.com>
2023-05-08 14:17:19 +0200

    user info component modified, it retrieves the data from current user and displays it

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -1,21 +1,59 @@
-import React from "react";
 import pic from "../img/profile-pic.jpg";
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { useEffect, useState } from "react";
+import { getAuth } from "firebase/auth";
+
+function getCurrentUserId() {
+  const auth = getAuth();
+  const currentUser = auth.currentUser;
+
+  if (currentUser) {
+    return currentUser.uid;
+  }
+
+  return null;
+}
 
 function UserInfo() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userId = getCurrentUserId();
+        if (userId) {
+          const userDocRef = doc(db, "users", userId);
+          const userDocSnap = await getDoc(userDocRef);
+
+          if (userDocSnap.exists()) {
+            const user = userDocSnap.data();
+            setUserData(user);
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  if (!userData) {
+    return null;
+  }
+
   return (
-    <div className="user_info">
-      {" "}
-      {/* basic user information: Name; Academic; Photo */}
-      <h3 className="absolute top-24 right-48 text-brown text-2xl">
-        Anna Woods
-      </h3>
-      <h3 className="absolute top-32 right-48  text-brown text-2xl">
-        Computer Science | Third year
-      </h3>
+    <div className="flex justify-end mr-5">
+      <div className="font-semibold text-blue-800 text-xl font-[Poppins]tracking-wide mr-3 mt-2 text-right">
+        <h3>{userData.name}</h3>
+        <h3>{userData.major}</h3>
+        <h3>{userData.yearOfStudy}</h3>
+      </div>
       <img
         src={pic}
         alt="pic"
-        className="md:cursor-pointer h-24 absolute top-20 right-20 w-24 rounded-full"
+        className="md:cursor-pointer h-24 mr-2 rounded-full top-20 right-20 w-24 my-2"
       />
     </div>
   );
