7a3b05d729e7d012dac9187d5d4cee66e9ac3279
Ajna <ajnaveladzic01@gmail.com>
2023-05-15 23:18:21 +0200

    course page and star rating component

--- src/pages/CoursePage.jsx
+++ src/pages/CoursePage.jsx
@@ -1,11 +1,42 @@
 import React from "react";
 import Header from "../components/Header";
+import PostView from "../components/PostView";
+import StarRating from "../components/StarRating";
+
+
 
 export default function CoursePage() {
   return (
     <>
       <Header />
-      <div>Course page</div>
+    
+      <div>Course page
+      <div className="flex items-center justify-center "> 
+      <h1> Course: Advanced Programming  </h1> 
+      </div>
+
+      <div className="flex items-center justify-center ">
+        <h1> Course Code: CS105 </h1>
+        </div>
+        <div> <br></br></div></div>
+
+           <div>
+           
+            <div className="flex items-row"><PostView /><StarRating /> <h1> Note name: Chapter 19: Nodes </h1></div>
+            <div> <br></br></div>
+            <div className="flex items-row"><PostView /> <StarRating /> <h1> Note name: Chapter 19: Nodes </h1></div>
+            <div> <br></br></div>
+            <div className="flex items-row"><PostView /><StarRating /> <h1> Note name: Chapter 19: Nodes </h1></div>
+            <div> <br></br></div>
+            <div className="flex items-row"><PostView /><StarRating /> <h1> Note name: Chapter 19: Nodes </h1></div>
+            <div> <br></br></div>
+            <div className="flex items-row"><PostView /><StarRating /> <h1> Note name: Chapter 19: Nodes </h1>  </div>
+
+            
+          </div>
+
+
+
     </>
   );
 }
